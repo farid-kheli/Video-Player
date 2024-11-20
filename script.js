966e5e5f9da6f1ba5bbfe8d22b3e809bc5d88c57
Moha18lrid <faridklc17@gmail.com>
2024-11-20 15:24:48 +0100

    add a sound controle bar that is a range input it css properties and add the js that controle the vulume and mute the video and unmute it

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let btn = document.getElementById('play-pause')
+let sound = document.getElementById('sound')
 let video = document.querySelector('.video')
 let bar = document.querySelector('.bar-juice')
+let soundBar = document.getElementById('sound-bar')
 function PlayPause(){
     if(video.paused){
-        btn.src='https://icons.veryicon.com/png/o/media/multimedia-flat-multicolor/pause-43.png'
+        btn.src='https://icons.veryicon.com/png/o/media/multi-media-2/pause-multimedia.png'
         video.play()
     }else{
-        btn.src='https://icons.veryicon.com/png/o/media/multimedia-flat-multicolor/play-74.png'
+        btn.src='https://icons.veryicon.com/png/o/media/multi-media-2/play-multimedia.png'
         video.pause()
     }
 }
@@ -14,3 +16,22 @@ video.addEventListener('timeupdate',function(){
     var barporsenteg = video.currentTime/video.duration;
     bar.style.width = barporsenteg * 100 + '%'
 })
+soundBar.addEventListener('input',function(){
+    //console.log(soundBar.value)
+    video.volume = soundBar.value/100;
+    if(video.volume==0){
+        sound.src='https://icons.veryicon.com/png/o/media/multi-media-2/mute-speaker-multimedia.png'
+    }
+    else{
+        sound.src='https://icons.veryicon.com/png/o/media/multi-media-2/sound-speaker-multimedia.png'
+    }
+})
+function MuteYN(){
+    if(video.muted){
+        sound.src='https://icons.veryicon.com/png/o/media/multi-media-2/sound-speaker-multimedia.png'
+        video.muted = false;
+    }else{
+        sound.src='https://icons.veryicon.com/png/o/media/multi-media-2/mute-speaker-multimedia.png'
+        video.muted = true;
+    }
+}
